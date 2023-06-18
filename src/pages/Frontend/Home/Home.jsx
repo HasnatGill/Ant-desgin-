1316166c34a9bd45ce597325e714e21aa3b9a989
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-06-18 13:22:43 +0500

    Tweaks

--- src/pages/Frontend/Home/Home.jsx
+++ src/pages/Frontend/Home/Home.jsx
@@ -1,39 +1,49 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
+import { Col, Row, Typography } from 'antd'
 import WebViewer from '@pdftron/webviewer';
 
+const { Title } = Typography
 
-export default function Home() {
-	const viewer = useRef(null);
+export default function Apryse() {
 
-	useEffect(() => {
-		WebViewer(
-			{
-				path: 'lib',
-				// licenseKey: 'demo:1687013927635:7d82999a03000000009017f03a9b78903e5513024c42cb0ca11b316894',
-				initialDoc: '/src/assets/file/document.pdf',
-				fullAPI: true,
-			},
-			viewer.current,
-		).then(instance => {
-			const { documentViewer, annotationManager } = instance.Core;
+    const viewer = useRef(null);
+    const [state, setState] = useState({ firstName: "", email: "" })
 
-			documentViewer.addEventListener('documentLoaded', () => {
-				documentViewer.getAnnotationsLoadedPromise().then(() => {
-					// iterate over fields
-					const fieldManager = annotationManager.getFieldManager();
-					fieldManager.forEachField(field => {
-						console.log(field.getValue(), "Value");
-						field.setValue('new value');
-					});
-				});
-			});
-		});
-	}, [])
+    useEffect(() => {
+        WebViewer(
+            {
+                path: '/lib',
+                licenseKey: 'demo:1684477490584:7da56d8f03000000005942e0782db834670ae612b1c3db07f5a4f9fae0',
+                initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+            },
+            viewer.current,
+        ).then((instance) => {
+            const { annotationManager } = instance.Core;
+            // you can now call WebViewer APIs here...
 
-	return (
-		<div>
-			<div className="header">React</div>
-			<div className="webviewer" ref={viewer} style={{ height: "100vh" }}></div>
-		</div>
-	);
+            annotationManager.addEventListener('fieldChanged', (field, value) => {
+                console.log('field', field)
+                console.log(`Field changed: ${field.name}, ${value}`);
+                setState(s => ({ ...s, [field.name]: value }))
+            });
+        });
+    }, []);
+
+    return (
+        <>
+            <Row gutter={[16]}>
+                <Col xs={12}>
+                    <Title level={3} className='text-blue fw-500'>Apryse</Title>
+                </Col>
+                <Col xs={12}>
+                    <Title level={3} className='text-blue fw-500'>Name: {state.firstName} - Email: {state.email}</Title>
+                </Col>
+            </Row>
+                    <div className="webviewer" ref={viewer} style={{minHeight:"90vh"}} ></div>
+            {/* <Row className='mt-0'>
+                <Col span={24}>
+                </Col>
+            </Row> */}
+        </>
+    )
 }
