31308a4c4ad938373d12b908bcd410793ee04e30
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-06-18 12:59:17 +0500

    Tweaks

--- src/pages/Frontend/Home/Home.jsx
+++ src/pages/Frontend/Home/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 			{
 				path: 'lib',
 				// licenseKey: 'demo:1687013927635:7d82999a03000000009017f03a9b78903e5513024c42cb0ca11b316894',
-				initialDoc: '/src/assets/file/',
+				initialDoc: '/src/assets/file/document.pdf',
 				fullAPI: true,
 			},
 			viewer.current,
@@ -32,7 +32,7 @@ export default function Home() {
 
 	return (
 		<div>
-			<div className="header">React sample</div>
+			<div className="header">React</div>
 			<div className="webviewer" ref={viewer} style={{ height: "100vh" }}></div>
 		</div>
 	);
