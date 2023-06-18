aa82f9b8a3c8aad249cd69d0dc3f43064c600bea
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-06-18 12:57:13 +0500

    add the intial pdf own project

--- src/pages/Frontend/Home/Home.jsx
+++ src/pages/Frontend/Home/Home.jsx
@@ -9,15 +9,26 @@ export default function Home() {
 		WebViewer(
 			{
 				path: 'lib',
-				licenseKey: 'demo:1687013927635:7d82999a03000000009017f03a9b78903e5513024c42cb0ca11b316894',
-				initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf',
+				// licenseKey: 'demo:1687013927635:7d82999a03000000009017f03a9b78903e5513024c42cb0ca11b316894',
+				initialDoc: '/src/assets/file/document.pdf',
+				fullAPI: true,
 			},
 			viewer.current,
-		).then((instance) => {
-			const { documentViewer } = instance.Core;
-			// you can now call WebViewer APIs here...
+		).then(instance => {
+			const { documentViewer, annotationManager } = instance.Core;
+
+			documentViewer.addEventListener('documentLoaded', () => {
+				documentViewer.getAnnotationsLoadedPromise().then(() => {
+					// iterate over fields
+					const fieldManager = annotationManager.getFieldManager();
+					fieldManager.forEachField(field => {
+						console.log(field.getValue(), "Value");
+						field.setValue('new value');
+					});
+				});
+			});
 		});
-	}, []);
+	}, [])
 
 	return (
 		<div>
