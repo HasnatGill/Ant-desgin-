e2a5d17fc2cbc468ca96fc9416e40b765a2d1993
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-07-07 10:23:37 +0500

    Tweaks

--- src/pages/Frontend/Success/Success.js
+++ src/pages/Frontend/Success/Success.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Success() {
     return (
         <div style={{ display: "flex", justifyContent: "center", marginTop:270, alignItems: "center" }}>
-            <h1>Your Payment is Success Programmer GIll </h1>
+            <h1>Your Payment is Success</h1>
         </div>
     )
 }
