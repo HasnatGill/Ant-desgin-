41ef60f283814643128e3ff4ad31ef71078ab882
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-07-08 16:07:10 +0500

    add the Navbar Component

---
+++ src/Component/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+             <h5 className='text-center pt-3'>Welcome to Redux Toolkit</h5>
+        </>
+    )
+}
--- src/pages/Frontend/Home/Home.jsx
+++ src/pages/Frontend/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export default function Home() {
-  return (
-    <>
-     <h5 className='text-center'>Welcome to Redux Toolkit</h5>   
-    </>
-  )
+    return (
+        <>
+
+
+        </>
+    )
 }
