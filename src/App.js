c33c357fc556af91165d0becd6ac7268be6dfb00
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-07-08 16:05:47 +0500

    Add the bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
 import './Apps.scss'
 import Routes from '../src/pages/Routes'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 
 function App() {
   return (
--- src/pages/Frontend/Home/Home.jsx
+++ src/pages/Frontend/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <>
-     <h1>This is Redux Toolkit</h1>   
+     <h5 className='text-center'>Welcome to Redux Toolkit</h5>   
     </>
   )
 }
