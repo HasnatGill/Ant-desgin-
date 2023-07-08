bb4e6da30aa68cce2085d004e5de64ad506d8f0c
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-07-08 20:39:23 +0500

    Tweaks

--- src/pages/Frontend/Home/Home.jsx
+++ src/pages/Frontend/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { DeleteOutlined } from '@ant-design/icons';
 
 export default function Home() {
     return (
-        <>
-        
+        <div className="container">
 
-        </>
+            <div className="d-flex justify-content-between align-items-center">
+                <h2>List of User</h2>
+                <button className='add-btn'>Add the User</button>
+            </div>
+            <div className="list_section d-flex py-3 px-5 justify-content-between align-items-center">
+                <h6>fullName</h6>
+            </div>
+        </div>
     )
 }
