97a329138d7962805a53f526aca53683ab35d18f
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-07-09 11:52:27 +0500

    Tweaks

--- src/pages/Frontend/Home/Home.jsx
+++ src/pages/Frontend/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AiFillDelete } from "react-icons/ai"
 
 export default function Home() {
     return (
@@ -8,10 +9,15 @@ export default function Home() {
                 <h2>List of User</h2>
                 <button className='add-btn'>Add the User</button>
             </div>
+
             <div className="list_section d-flex py-3 px-5 justify-content-between align-items-center">
                 <h6>fullName</h6>
-                <h6>Delete</h6>
+                <span><AiFillDelete className='fs-5' /></span>
             </div>
+
+
+
         </div>
-    )
+    )    
 }
+
