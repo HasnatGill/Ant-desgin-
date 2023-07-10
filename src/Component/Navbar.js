de89be932509458b982b143687d49674d0789a16
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-07-10 15:05:44 +0500

    Tweaks

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
     return (
         <>
-            <h5 className='text-center pt-3'>WELCOME to Redux Toolkit</h5>
+            <h5 className='text-center pt-3'>WELCOME TO Redux Toolkit</h5>
             <h1 className='text-center text-secondary display-2 fw-bold'>ADMIN TABLE</h1>
 
             <ul style={{ listStyle: "none", display: "flex" }} className='text-center bg-secondary justify-content-center my-4 text-white py-3'>
