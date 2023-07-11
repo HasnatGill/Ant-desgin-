1934afd12978c298e7d15e600f6867b2a16a5ec1
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-07-11 19:32:57 +0500

    tweaks

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
             <h5 className='text-center pt-3'>WELCOME TO REDUX TOOLKIT</h5>
             <h1 className='text-center text-secondary display-2 fw-bold'>ADMIN TABLE</h1>
 
-            <ul style={{ listStyle: "none", display: "flex" }} className='text-center bg-dark justify-content-center my-4 text-white py-3'>
+            <ul style={{ listStyle: "none", display: "flex" }} className='text-center bg-secondary justify-content-center my-4 text-white py-3'>
                 <li className='px-3 pointer-cursor'>HOME</li>
                 <li className='px-3 pointer-cursor'>ABOUT</li>
                 <li className='px-3 pointer-cursor'>PROJECTS</li>
